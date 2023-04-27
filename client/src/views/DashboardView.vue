f6d3eb170245cfc55be0d52c229626b6c4ce101b
chrislo <chris@soychristian.com>
2023-04-27 13:55:54 -0500

    frontend views done

---
+++ client/src/views/DashboardView.vue
@@ -0,0 +1,448 @@
+<script setup>
+</script>
+
+
+<template>
+  <header class="navbar">
+      <div class="">
+        <h1>Car Vendor</h1>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Cars</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section id="banner" class="banner">
+        <h2>Welcome to Car Vendor</h2>
+        <p>
+          We offer a wide selection of high-quality cars at competitive prices.
+        </p>
+        <a href="#" class="btn">View Our Cars</a>
+      </section>
+      <section id="featured-cars" class="featured-cars-section">
+        <h2>Featured Cars</h2>
+        <div class="featured-cars">
+
+          <div class="car">
+            <!-- <img src="car1.jpg" alt="Car 1" class="car-image" /> -->
+            <div class="car-image car1"></div>
+            <h3>2021 Toyota Camry</h3>
+            <p>$25,000</p>
+            <a href="#" class="btn">Learn More</a>
+          </div>
+          <div class="car">
+            <!-- <img src="car2.jpg" alt="Car 2" class="car-image" /> -->
+            <div class="car-image car2"></div>
+            <h3>2021 Honda Accord</h3>
+            <p>$26,000</p>
+            <a href="#" class="btn">Learn More</a>
+          </div>
+          <div class="car">
+            <!-- <img src="car3.jpg" alt="Car 3" class="car-image" /> -->
+            <div class="car-image car3"></div>
+            <h3>2021 Ford Mustang</h3>
+            <p>$35,000</p>
+            <a href="#" class="btn">Learn More</a>
+          </div>
+          <div class="car">
+            <!-- <img src="car3.jpg" alt="Car 3" class="car-image" /> -->
+            <div class="car-image car3"></div>
+            <h3>2021 Ford Mustang</h3>
+            <p>$35,000</p>
+            <a href="#" class="btn">Learn More</a>
+          </div>
+        </div>
+      </section>
+      <section id="about" class="about">
+        <h2>About Us</h2>
+        <p>
+          We are a family-owned car dealership that has been in business for
+          over 20 years. We are committed to providing our customers with the
+          best possible car buying experience.
+        </p>
+        <a href="#" class="btn">Learn More</a>
+      </section>
+      <section id="contact" class="contact">
+        <div class="">
+          <h2>Contact Us</h2>
+        </div>
+        <form action="submit-form.php" method="post">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+          <label for="message">Message:</label>
+          <textarea id="message" name="message" required></textarea>
+          <button type="submit" class="btn">Send Message</button>
+        </form>
+      </section>
+    </main>
+    <footer>
+      <p>Car Vendor &copy; 2023</p>
+    </footer>
+</template>
+
+
+<style scoped>
+/* Estilos Globales */
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+  color: #000;
+}
+
+/* Estilos Barra de Navegación */
+
+.navbar {
+  background-color: #ffffff;
+  font-size: 20px;
+  font-weight: bold;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+}
+.navbar div {
+  display: flex;
+  align-items: center;
+  height: 100%;
+}
+.navbar div h1 {
+  font-size: 30px;
+  font-weight: bold;
+  color: #000000;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+
+.navbar nav ul {
+  display: flex;
+  list-style: none;
+}
+
+.navbar nav ul li {
+  margin-left: 20px;
+}
+
+.navbar nav ul li a {
+  color: #000000;
+
+  /* &:hover {
+    color: #000;
+  } */
+}
+
+.navbar nav ul li a:hover {
+  color: #6b6b6b;
+}
+
+/* Estilos Banner */
+
+.banner {
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-image: url(../assets/background.jpg);
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+}
+
+.banner h2 {
+  font-size: 50px;
+  font-weight: bold;
+  color: #ffffff;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  text-align: center;
+  display: inline;
+}
+
+.banner p {
+  font-size: 20px;
+  color: #ffffff;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  text-align: center;
+}
+
+.banner a {
+  background-color: #757575;
+  padding: 10px 20px;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 20px;
+}
+
+.banner a:hover {
+  background-color: #fff;
+  color: #000000;
+}
+
+/* Featured Cars & Cars Section */
+
+.featured-cars {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 50px;
+}
+
+.featured-cars-section h2 {
+  font-size: 30px;
+  font-weight: bold;
+  color: #000000;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  margin-top: 20px;
+  text-align: center;
+
+}
+
+.featured-cars .car {
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+
+}
+
+.car1 {
+  background-image: url(../assets/toyota-camry.jpg);
+}
+
+.car2 {
+  background-image: url(../assets/honda-accord.png);
+}
+
+.car3 {
+  background-image: url(../assets/ford-mustang.jpg);
+}
+
+.featured-cars .car .car-image {
+  /* overflow: hidden; */
+  border-radius: 10px 10px 0 0;
+  width: 400px;
+  height: 170px;
+  background-color: #6b6b6b;
+
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+}
+
+
+
+.featured-cars .car h3 {
+  font-size: 20px;
+  font-weight: bold;
+  color: #000000;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  margin: 15px 0;
+}
+
+.featured-cars .car p {
+  font-size: 20px;
+  color: #000000;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  margin-bottom: 10px;
+}
+
+.featured-cars .car a {
+  background-color: #757575;
+  padding: 10px 20px;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.featured-cars .car a:hover {
+  background-color: #000;
+  color: #fff;
+}
+
+
+/* About us section */
+
+.about {
+  display: flex;
+  flex-direction: column;
+  color: white;
+  justify-content: space-between;
+  align-items: center;
+  padding: 50px;
+  background-image: url(../assets/background.jpg);
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+}
+
+.about h2 {
+  font-size: 30px;
+  font-weight: bold;
+  color: #ffffff;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  margin-bottom: 20px;
+}
+
+.about p {
+  font-size: 18px;
+  color: #ffffff;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  text-align: justify;
+  margin-bottom: 20px;
+}
+
+.about .btn {
+  background-color: #757575;
+  padding: 10px 20px;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.about .btn:hover {
+  background-color: #000;
+  color: #fff;
+}
+
+/* Contact */
+.contact {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 50px;
+}
+
+.contact div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* border-right: 1px solid #000; */
+  padding: 0 50px;
+  margin-bottom: 20px;
+}
+
+.contact div h2 {
+  font-size: 30px;
+  font-weight: bold;
+  color: #000000;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+.contact form {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.contact form input,
+.contact form textarea {
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
+  border: 1px solid #000;
+  resize: none;
+}
+
+.contact form input:focus,
+.contact form textarea:focus {
+  outline: none;
+  border: 1px solid #757575;
+}
+
+.contact form input {
+  width: 300px;
+}
+
+.contact form textarea {
+  width: 300px;
+  height: 200px;
+}
+
+.contact form button {
+  background-color: #757575;
+  padding: 10px 20px;
+  border-radius: 5px;
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 20px;
+}
+
+.contact form button:hover {
+  background-color: #000;
+  cursor: pointer;
+}
+
+/* Footer */
+
+footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  background-color: #000;
+  color: #fff;
+}
+
+footer p {
+  font-size: 18px;
+  font-weight: bold;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+@media screen and (min-width: 960px){
+  .featured-cars {
+    flex-direction: row; 
+    flex-wrap: wrap;
+  }
+
+  .about p {
+    width: 80%;
+  }
+
+  .contact {
+    flex-direction: row;
+  }
+  
+  .contact h2 {
+    padding: 20px;
+    border-right: 1px solid #000;
+  }
+}
+
+@media screen and (min-width: 1200px){
+  .about {
+    flex-direction: row;
+  }
+  .about p {
+    width: 900px;
+    text-align: center;
+  }
+}
+
+</style>
+
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,177 @@
+<script setup>
+</script>
+
+
+<template>
+  <div class="container">
+      <div class="info">
+        <p class="txt-1">Gracias por visitarnos</p>
+        <h2>bienvenido de nuevo</h2>
+        <hr />
+        <p class="txt-2">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
+          mollitia velit fugit porro, accusamus aperiam at quaerat laudantium
+          sapiente vel esse autem enim corrupti maiores expedita provident
+          veritatis nihil optio.
+        </p>
+      </div>
+      <form class="form">
+        <h2>login</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa vitae
+          magnam hic temporibus repudiandae voluptatem libero praesentium id
+          nihil at reiciendis, maxime magni obcaecati ea, exercitationem
+          voluptatibus cupiditate optio? Dolorem.
+        </p>
+        <div class="inputs">
+          <input type="email" class="box" placeholder="Ingresa tu correo" />
+          <input
+            type="password"
+            class="box"
+            placeholder="Ingresa tu contraseña"
+          />
+          <a href="#">Olvidaste tu contraseña?</a>
+          <input type="submit" value="login" class="submit" />
+        </div>
+      </form>
+    </div>
+</template>
+
+
+<style scoped>
+body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: #e5ecf9;
+}
+
+
+
+.container {
+    display: flex;
+    height: 700px;
+    width: 1000px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
+    margin: auto;
+    margin-top: 150px;
+}
+
+.info{
+    background-image: url(../assets/bg.jpg);
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    text-align: center;
+    padding: 150px 50px;
+    width: 50%; 
+}
+
+.txt-1 {
+    color: #fff;
+    text-transform: capitalize;
+    font-size: 20px;
+    margin-bottom: 25px;
+}
+
+.info h2 {
+    color: #fff;
+    text-transform: uppercase;
+    font-size: 50px;
+    margin-bottom: 25px;
+}
+
+.info hr {
+    width: 55px;
+    border: 4px solid #fff;
+    margin-left: 170px;
+    margin-bottom: 25px;
+}
+
+.txt-2 {
+    color: #fff;
+    font-size: 18px;
+}
+
+.form {
+    padding: 150px 100px;
+    width: 50%;
+    text-align: center;
+    background-color: #fff;
+}
+
+.form h2 {
+    color: #097ef6;
+    font-size: 30px;
+    margin-bottom: 25px;
+}
+
+.form p {
+    font-size: 16px;
+    color: #c4c3c3;
+    margin-bottom: 25px;
+}
+
+.inputs {
+    display: flex;
+    flex-direction: column;
+}
+
+.box {
+    outline: none;
+    border-color: #097ef6;
+    border-width: 0px 0px 0px 5px;
+    border-style: solid;
+    padding: 15px 35px;
+    margin-bottom: 20px;
+    background-color: #f9f9f9;
+}
+
+.inputs a {
+
+    color: #097ef6;
+    text-decoration: none;
+    font-size: 15px;
+    margin-bottom: 35px;
+
+}
+
+.submit {
+
+    background-color: #097ef6;
+    padding: 15px;
+    border: 0;
+    color: #fff;
+    font-size: 18px;
+    text-transform: uppercase;
+    border-radius: 25px;
+
+
+}
+
+@media(max-width:990px) {
+
+    .container {
+        width: 350px;
+        height: 600px;
+        margin-top: 100px;
+    }
+
+    .info {
+        display: none;
+    }
+
+    .form {
+        /* width: 100px;  */
+        width: 100%;
+        padding: 30px;
+    }
+
+    .form h2 {
+        margin-top: 50px;
+    }
+
+
+}
+</style>
